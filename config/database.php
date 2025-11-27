ddde695247106ac3bd28130cd69240ba19956cd3
phucchill <quanglephuct@gmail.com>
2025-11-27 17:35:06 +0700

    sua de deploy

--- config/database.php
+++ config/database.php
@@ -57,9 +57,6 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            // 'options' => extension_loaded('pdo_mysql') ? array_filter([
-            //     PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            // ]) : [],
             'options' => extension_loaded('pdo_mysql') ? [] : [],
         ],
 
@@ -78,9 +75,7 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => extension_loaded('pdo_mysql') ? [] : [],
         ],
 
         'pgsql' => [
